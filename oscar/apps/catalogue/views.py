6a16cfce974d6a613e4b155a98c78cb1018b0e2d
ZhenyuGeng <freergzy8212@hotmail.com>
2019-09-05 22:46:48 +0200

    update

--- oscar/apps/catalogue/views.py
+++ oscar/apps/catalogue/views.py
@@ -145,11 +145,8 @@ class CatalogueView(TemplateView):
         ctx.update(search_context)
         return ctx
 
-
+'''
 class SubCatalogueAndDetailView(TemplateView):
-    """
-    Browse products in a given category
-    """
     context_object_name = "products"
     template_name = 'oscar/catalogue/category.html'
     enforce_paths = True
@@ -170,6 +167,63 @@ class SubCatalogueAndDetailView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['category'] = self.category
         context['descendants'] = self.category.get_descendants()
+        return context
+'''
+
+
+class SubCatalogueAndDetailView(TemplateView):
+    """
+    Browse products in a given category
+    """
+    context_object_name = "products"
+    template_name = 'oscar/catalogue/category.html'
+    enforce_paths = True
+
+    def get(self, request, *args, **kwargs):
+        # Fetch the category; return 404 or redirect as needed
+        self.category = self.get_category()
+        potential_redirect = self.redirect_if_necessary(
+            request.path, self.category)
+        if potential_redirect is not None:
+            return potential_redirect
+
+        try:
+            self.search_handler = self.get_search_handler(
+                request.GET, request.get_full_path(), self.get_categories())
+        except InvalidPage:
+            messages.error(request, _('The given page number was invalid.'))
+            return redirect(self.category.get_absolute_url())
+
+        return super().get(request, *args, **kwargs)
+
+    def get_category(self):
+        return get_object_or_404(Category, pk=self.kwargs['pk'])
+
+    def redirect_if_necessary(self, current_path, category):
+        if self.enforce_paths:
+            # Categories are fetched by primary key to allow slug changes.
+            # If the slug has changed, issue a redirect.
+            expected_path = category.get_absolute_url()
+            if expected_path != urlquote(current_path):
+                return HttpResponsePermanentRedirect(expected_path)
+
+    def get_search_handler(self, *args, **kwargs):
+        return get_product_search_handler_class()(*args, **kwargs)
+
+    def get_categories(self):
+        """
+        Return a list of the current category and its ancestors
+        """
+        categories_list = self.category.get_descendants_and_self()
+        return categories_list
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.category
+        context['descendants'] = self.category.get_descendants()
+        search_context = self.search_handler.get_search_context_data(
+            self.context_object_name)
+        context.update(search_context)
         return context
 
 class ProductCategoryView(TemplateView):
